f564dac777de3cd037d4bc4baa0f0c36d31f8cbb
Monochronix <kylehuangmonochronix@gmail.com>
2025-01-20 10:19:02 -0800

    Mobile navbar expanded icons fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,6 @@
 import './Navbar.css'
 import Link from 'next/link'
 import { useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faBriefcase, faFeather, faArrowTrendUp } from '@fortawesome/free-solid-svg-icons';
 import Image from 'next/image';
 
 const Navbar = () => {
@@ -16,21 +14,21 @@ const Navbar = () => {
   return (
     <>
       <nav className={expanded ? 'expanded' : ''}>
-        <Link href="/" className='nav-link home'><Image src="/icons/home.svg" className='home-icon' alt="Home Icon" width={18} height={18} />Home</Link>
+        <Link href="/" className='nav-link home'><Image src="/icons/home.svg" className="home-icon" alt="Home Icon" width={18} height={18} />Home</Link>
         <div className='separator'></div>
         <Link href="/portfolio" className='nav-link portfolio'>Portfolio</Link>
         <Link href="/hermien" className='nav-link hermien'>Hermien</Link>
         <Link href="/blog" className='nav-link blog'>Blog</Link>
-        <Link href="/contact" className='contact'><Image src="/icons/envelope.svg" className='contact-icon' alt="Envelope Icon" width={18} height={18} />Contact</Link>
+        <Link href="/contact" className='contact'><Image src="/icons/envelope.svg" className="contact-icon" alt="Envelope Icon" width={18} height={18} />Contact</Link>
         <div className="menu-btn" role="button" onClick={toggleNavbar}>
           <div className={`menu-btn-icon ${expanded ? 'animate-menu-icon' : ''}`}></div>
         </div>
       </nav>
       <div className={`expanded-content ${expanded ? 'reveal-expanded' : ''}`}>
-        <Link href="/portfolio" className="expanded-link"><FontAwesomeIcon icon={faBriefcase} className="expanded-link-icon" />Portfolio</Link>
-        <Link href="/hermien" className="expanded-link"><FontAwesomeIcon icon={faArrowTrendUp} className="expanded-link-icon" />Hermien</Link>
-        <Link href="/blog" className="expanded-link"><FontAwesomeIcon icon={faFeather} className="expanded-link-icon" />Blog</Link>
-        <Link href="/contact" className="expanded-link"><FontAwesomeIcon icon={faEnvelope} className="expanded-link-icon" />Contact</Link>
+        <Link href="/portfolio" className="expanded-link"><Image src="/icons/briefcase.svg" className="expanded-link-icon" alt="Briefcase Icon" width={16} height={16} />Portfolio</Link>
+        <Link href="/hermien" className="expanded-link"><Image src="/icons/arrow-trend-up.svg" className="expanded-link-icon" alt="Upwards Trend Arrow Icon" width={16} height={16} />Hermien</Link>
+        <Link href="/blog" className="expanded-link"><Image src="/icons/feather.svg" className="expanded-link-icon" alt="Feather Icon" width={16} height={16} />Blog</Link>
+        <Link href="/contact" className="expanded-link"><Image src="/icons/envelope.svg" className="expanded-link-icon" alt="Envelope Icon" width={16} height={16} />Contact</Link>
       </div>
     </>
   );
