3864bc56e7517ecccdcefb8758ae4c5250d050fc
Monochronix <kylehuangmonochronix@gmail.com>
2025-01-20 13:49:44 -0800

    Added metadata with open graph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
 import './Home.css';
 import Head from 'next/head'
+import type { Metadata } from 'next'
 
-export const metadata = {
-  title: 'Kyle Huang | Creator & Polymath'
-};
+export const metadata: Metadata = {
+  title: 'Kyle Huang | Creator & Polymath',
+  description: 'Kyle Huang is a creator and a polymath, with an interest in finance and economics.',
+  openGraph: {
+    title: 'Kyle Huang | Creator & Polymath',
+    description: 'Kyle Huang is a creator and a polymath, with an interest in finance and economics.',
+    images: 'https://kyle-huang.vercel.app/images/OpenGraphImage.png',
+    url: 'https://kyle-huang.vercel.app',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Kyle Huang | Creator & Polymath',
+    description: 'Kyle Huang is a creator and a polymath, with an interest in finance and economics.',
+    images: 'https://kyle-huang.vercel.app/images/OpenGraphImage.png',
+  },
+}
 
 const Home = () => {
   return(
