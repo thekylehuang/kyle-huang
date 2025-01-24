bfad68553a14b81fe957e7c0e36baff2b519a27a
Monochronix <kylehuangmonochronix@gmail.com>
2025-01-23 22:31:56 -0800

    Navbar full revamp

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 "use client"
 
-import { Geist } from 'next/font/google'
-import './Navbar.css'
-import Link from 'next/link'
+import { Geist } from 'next/font/google';
+import './Navbar.css';
+import Link from 'next/link';
 import { useState } from "react";
-import Image from 'next/image';
+import { Mail, House, BriefcaseBusiness, TrendingUp, Feather, LogIn, UserPlus } from 'lucide-react';
+
 
 const geist = Geist({ subsets: ['latin'] })
 
@@ -16,22 +17,29 @@ const Navbar = () => {
   };
   return (
     <>
-      <nav className={`${geist.className} ${expanded ? "expanded" : ""}`}>
-        <Link href="/" className='nav-link home'><Image src="/icons/home.svg" className="home-icon" alt="Home Icon" width={18} height={18} />Home</Link>
-        <div className='separator'></div>
-        <Link href="/portfolio" className='nav-link portfolio'>Portfolio</Link>
-        <Link href="/hermien" className='nav-link hermien'>Hermien</Link>
-        <Link href="/blog" className='nav-link blog'>Blog</Link>
-        <Link href="/contact" className='contact'><Image src="/icons/envelope.svg" className="contact-icon" alt="Envelope Icon" width={18} height={18} />Contact</Link>
-        <div className="menu-btn" role="button" onClick={toggleNavbar}>
-          <div className={`menu-btn-icon ${expanded ? 'animate-menu-icon' : ''}`}></div>
-        </div>
-      </nav>
+      <div className="navbar">
+        <Link href="/" className={`${geist.className} absolute font-bold left-8 z-10 text-3xl top-5 hidden lg:inline-block`}>Kyle Huang</Link>
+        <nav className={`${geist.className} ${expanded ? "expanded" : ""} center-navbar`}>
+          <Link href="/" className='nav-link home'><House className='home-icon' size={20}/>Home</Link>
+          <div className='separator'></div>
+          <Link href="/portfolio" className='nav-link portfolio'>Portfolio</Link>
+          <Link href="/hermien" className='nav-link hermien'>Hermien</Link>
+          <Link href="/blog" className='nav-link blog'>Blog</Link>
+          <Link href="/contact" className='contact'><Mail className="contact-icon" size={20}/>Contact</Link>
+          <div className="menu-btn" role="button" onClick={toggleNavbar}>
+            <div className={`menu-btn-icon ${expanded ? 'animate-menu-icon' : ''}`}></div>
+          </div>
+        </nav>
+        <Link href="/login" className={`${geist.className} absolute font-semibold top-5 bg-neutral-800 px-4 py-1 rounded-lg text-neutral-200 border border-neutral-700 hover:border-neutral-600 right-36 hidden lg:inline-block transition-colors`}>Login</Link>
+        <Link href="/signup" className={`${geist.className} absolute right-8 top-5 font-semibold bg-violet-950 px-4 py-1 rounded-lg border border-violet-800 hover:border-violet-700 hidden lg:inline-block transition-colors`}>Sign Up</Link>
+      </div>
       <div className={`expanded-content ${expanded ? 'reveal-expanded' : ''}`}>
-        <Link href="/portfolio" className="expanded-link"><Image src="/icons/briefcase.svg" className="expanded-link-icon" alt="Briefcase Icon" width={16} height={16} />Portfolio</Link>
-        <Link href="/hermien" className="expanded-link"><Image src="/icons/arrow-trend-up.svg" className="expanded-link-icon" alt="Upwards Trend Arrow Icon" width={16} height={16} />Hermien</Link>
-        <Link href="/blog" className="expanded-link"><Image src="/icons/feather.svg" className="expanded-link-icon" alt="Feather Icon" width={16} height={16} />Blog</Link>
-        <Link href="/contact" className="expanded-link"><Image src="/icons/envelope.svg" className="expanded-link-icon" alt="Envelope Icon" width={16} height={16} />Contact</Link>
+        <Link href="/portfolio" className="expanded-link"><BriefcaseBusiness className='expanded-link-icon' size={20} />Portfolio</Link>
+        <Link href="/hermien" className="expanded-link"><TrendingUp className='expanded-link-icon' size={20} />Hermien</Link>
+        <Link href="/blog" className="expanded-link"><Feather className='expanded-link-icon' size={20} />Blog</Link>
+        <Link href="/contact" className="expanded-link"><Mail className='expanded-link-icon' size={20} />Contact</Link>
+        <Link href="/login" className="expanded-link"><LogIn className='expanded-link-icon' size={20} />Login</Link>
+        <Link href="/signup" className="expanded-link"><UserPlus className='expanded-link-icon' size={20} />Sign Up</Link>
       </div>
     </>
   );
