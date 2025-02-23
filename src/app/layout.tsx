beae3d8d08a4b406dd04266d5706ebb6bc221190
Monochronix <kylehuangmonochronix@gmail.com>
2025-02-22 18:54:22 -0800

    Added suspense tags in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Footer from "@/components/layout/Footer"
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { ThemeProvider } from '@/components/layout/theme-provider'
 import { Analytics } from "@vercel/analytics/react"
-import React from "react"
+import React, { Suspense } from "react"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -13,7 +13,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <body className="antialiased dark:bg-neutral-900 flex flex-col gap-0">
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
             <Navbar />
-            {children}
+            <Suspense>
+              {children}
+            </Suspense>
             <Analytics />
             <Footer />
           </ThemeProvider>
